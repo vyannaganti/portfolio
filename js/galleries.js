8df742b4cb91d7d1afdbe26b1460e8e616b35403
vyannaganti <103096961+vyannaganti@users.noreply.github.com>
2022-04-12 08:13:36 +0530

    Add files via upload

---
+++ js/galleries.js
@@ -0,0 +1,320 @@
+// GALLERY 1
+jQuery('#gallery1').on('click', function (e) {
+    "use strict";
+    e.preventDefault();
+    jQuery(this).lightGallery({
+        // Settings
+        dynamic: true,
+        mode: 'lg-slide-vertical',
+        zoom: true,
+        fullScreen: true,
+        autoplay: false,
+        thumbnail: true,
+        download: true,
+        counter: true,
+        // Images
+        dynamicEl: [{
+            'src': 'images/web/op/01.png',
+            'thumb': 'images/web/op/01.png',
+            'subHtml': 'IP & OP Dashboard'
+        }, {
+            'src': 'images/web/op/02.PNG',
+            'thumb': 'images/web/op/02.PNG',
+            'subHtml': "Doctor Visit Details"
+        }, {
+            'src': 'images/web/op/03.PNG',
+            'thumb': 'images/web/op/03.PNG',
+            'subHtml': "Book Appointment"
+        }, {
+            'src': 'images/web/op/04.JPG',
+            'thumb': 'images/web/op/04.JPG',
+            'subHtml': "Visit Details"
+        }, {
+            'src': 'images/web/op/05.JPG',
+            'thumb': 'images/web/op/05.JPG',
+            'subHtml': "Manage Appointment"
+        }, {
+            'src': 'images/web/op/07.JPG',
+            'thumb': 'images/web/op/07.JPG',
+            'subHtml': "Quick Appointment"
+        }, {
+            'src': 'images/web/op/08.JPG',
+            'thumb': 'images/web/op/08.JPG',
+            'subHtml': "Admin Dashboard"
+        }, {
+            'src': 'images/web/op/09.JPG',
+            'thumb': 'images/web/op/09.JPG',
+            'subHtml': "Docotr Creation"
+        }, {
+            'src': 'images/web/op/10.png',
+            'thumb': 'images/web/op/10.png',
+            'subHtml': "Smart Report"
+        },{
+            'src': 'images/web/op/11.png',
+            'thumb': 'images/web/op/11.png',
+            'subHtml': "Smart Report 2"
+        }]
+    })
+});
+
+// GALLERY 2
+jQuery('#gallery2').on('click', function (e) {
+    "use strict";
+    e.preventDefault();
+    jQuery(this).lightGallery({
+        // Settings
+        dynamic: true,
+        mode: 'lg-slide-vertical',
+        zoom: true,
+        fullScreen: true,
+        autoplay: false,
+        thumbnail: true,
+        download: true,
+        counter: true,
+        // Images
+        dynamicEl: [{
+            'src': 'images/web/ip/01.JPG',
+            'thumb': 'images/web/ip/01.JPG',
+            'subHtml': "IP Login Page"
+        }, {
+            'src': 'images/web/ip/02.PNG',
+            'thumb': 'images/web/ip/02.PNG',
+            'subHtml': "Patients View"
+        }, {
+            'src': 'images/web/ip/03.PNG',
+            'thumb': 'images/web/ip/03.PNG',
+            'subHtml': "IP Dashboard"
+        }, {
+            'src': 'images/web/ip/04.PNG',
+            'thumb': 'images/web/ip/04.PNG',
+            'subHtml': "ER Assesement"
+        }, {
+            'src': 'images/web/ip/05.JPG',
+            'thumb': 'images/web/ip/05.JPG',
+            'subHtml': "Medication"
+        }, {
+            'src': 'images/web/ip/10.png',
+            'thumb': 'images/web/ip/10.png',
+            'subHtml': "Expditure"
+        }]
+    })
+});
+
+// GALLERY 3
+jQuery('#gallery3').on('click', function (e) {
+    "use strict";
+    e.preventDefault();
+    jQuery(this).lightGallery({
+        // Settings
+        dynamic: true,
+        mode: 'lg-slide-vertical',
+        zoom: true,
+        fullScreen: true,
+        autoplay: false,
+        thumbnail: true,
+        download: true,
+        counter: true,
+        // Images
+        dynamicEl: [{
+            'src': 'images/web/qms/01.png',
+            'thumb': 'images/web/qms/01.png',
+            'subHtml': "Admin Page"
+        }, {
+            'src': 'images/web/qms/02.png',
+            'thumb': 'images/web/qms/02.png',
+            'subHtml': "Admin Page"
+        }, {
+            'src': 'images/web/qms/03.png',
+            'thumb': 'images/web/qms/03.png',
+            'subHtml': "Nurse Station Dashboard"
+        }, {
+            'src': 'images/web/qms/04.png',
+            'thumb': 'images/web/qms/04.png',
+            'subHtml': "Nurse Station Dashboard"
+        }]
+    })
+});
+
+// GALLERY 4
+jQuery('#gallery4').on('click', function (e) {
+    "use strict";
+    e.preventDefault();
+    jQuery(this).lightGallery({
+        // Settings
+        dynamic: true,
+        mode: 'lg-slide-vertical',
+        zoom: true,
+        fullScreen: true,
+        autoplay: false,
+        thumbnail: true,
+        download: true,
+        counter: true,
+        // Images
+        dynamicEl: [{
+            'src': 'images/mobile/pp/01.png',
+            'thumb': 'images/mobile/pp/01.png',
+            'subHtml': "Landing Page"
+        }, {
+            'src': 'images/mobile/pp/02.png',
+            'thumb': 'images/mobile/pp/02.png',
+            'subHtml': "Find a Doctor"
+        }, {
+            'src': 'images/mobile/pp/03.png',
+            'thumb': 'images/mobile/pp/03.png',
+            'subHtml': "doctor Slots"
+        }, {
+            'src': 'images/mobile/pp/04.png',
+            'thumb': 'images/mobile/pp/04.png',
+            'subHtml': "Doctor Profile"
+        }]
+    })
+});
+
+// GALLERY 4
+jQuery('#gallery5').on('click', function (e) {
+    "use strict";
+    e.preventDefault();
+    jQuery(this).lightGallery({
+        // Settings
+        dynamic: true,
+        mode: 'lg-slide-vertical',
+        zoom: true,
+        fullScreen: true,
+        autoplay: false,
+        thumbnail: true,
+        download: true,
+        counter: true,
+        // Images
+        dynamicEl: [{
+            'src': 'images/mobile/op/1.png',
+            'thumb': 'images/mobile/op/1.png',
+            'subHtml': "Landing Page"
+        }, {
+            'src': 'images/mobile/op/2.png',
+            'thumb': 'images/mobile/op/2.png',
+            'subHtml': "Login"
+        }, {
+            'src': 'images/mobile/op/3.png',
+            'thumb': 'images/mobile/op/3.png',
+            'subHtml': "Main Menu"
+        }, {
+            'src': 'images/mobile/op/4.png',
+            'thumb': 'images/mobile/op/4.png',
+            'subHtml': "Consults"
+        }, {
+            'src': 'images/mobile/op/5.png',
+            'thumb': 'images/mobile/op/5.png',
+            'subHtml': "Server Error"
+        }, {
+            'src': 'images/mobile/op/6.png',
+            'thumb': 'images/mobile/op/6.png',
+            'subHtml': "Patient Details"
+        }, {
+            'src': 'images/mobile/op/7.png',
+            'thumb': 'images/mobile/op/7.png',
+            'subHtml': "Book Appointment"
+        }, {
+            'src': 'images/mobile/op/8.png',
+            'thumb': 'images/mobile/op/8.png',
+            'subHtml': "Patient Search"
+        }, {
+            'src': 'images/mobile/op/9.png',
+            'thumb': 'images/mobile/op/9.png',
+            'subHtml': "Patient Search"
+        }, {
+            'src': 'images/mobile/op/10.png',
+            'thumb': 'images/mobile/op/10.png',
+            'subHtml': "Visit Details"
+        }, {
+            'src': 'images/mobile/op/11.png',
+            'thumb': 'images/mobile/op/11.png',
+            'subHtml': "Dx"
+        }, {
+            'src': 'images/mobile/op/12.png',
+            'thumb': 'images/mobile/op/12.png',
+            'subHtml': "Rx"
+        }, {
+            'src': 'images/mobile/op/13.png',
+            'thumb': 'images/mobile/op/13.png',
+            'subHtml': "Order Set"
+        }, {
+            'src': 'images/mobile/op/14.png',
+            'thumb': 'images/mobile/op/14.png',
+            'subHtml': "Most Used"
+        }, {
+            'src': 'images/mobile/op/15.png',
+            'thumb': 'images/mobile/op/15.png',
+            'subHtml': "Assessment"
+        }, {
+            'src': 'images/mobile/op/16.png',
+            'thumb': 'images/mobile/op/16.png',
+            'subHtml': "History"
+        }, {
+            'src': 'images/mobile/op/17.png',
+            'thumb': 'images/mobile/op/17.png',
+            'subHtml': "History"
+        }]
+    })
+});
+
+// GALLERY 4
+jQuery('#gallery6').on('click', function (e) {
+    "use strict";
+    e.preventDefault();
+    jQuery(this).lightGallery({
+        // Settings
+        dynamic: true,
+        mode: 'lg-slide-vertical',
+        zoom: true,
+        fullScreen: true,
+        autoplay: false,
+        thumbnail: true,
+        download: true,
+        counter: true,
+        // Images
+        dynamicEl: [{
+            'src': 'images/mobile/intalk/1.png',
+            'thumb': 'images/mobile/intalk/1.png',
+            'subHtml': "Login"
+        }, {
+            'src': 'images/mobile/intalk/2.png',
+            'thumb': 'images/mobile/intalk/2.png',
+            'subHtml': "Dashboard"
+        }, {
+            'src': 'images/mobile/intalk/3.png',
+            'thumb': 'images/mobile/intalk/3.png',
+            'subHtml': "Ticket Info"
+        }, {
+            'src': 'images/mobile/intalk/4.png',
+            'thumb': 'images/mobile/intalk/4.png',
+            'subHtml': "Filter"
+        }, {
+            'src': 'images/mobile/intalk/5.png',
+            'thumb': 'images/mobile/intalk/5.png',
+            'subHtml': "Ticket Info"
+        }, {
+            'src': 'images/mobile/intalk/6.png',
+            'thumb': 'images/mobile/intalk/6.png',
+            'subHtml': "Ticket Info"
+        }, {
+            'src': 'images/mobile/intalk/7.png',
+            'thumb': 'images/mobile/intalk/7.png',
+            'subHtml': "Attachment"
+        }, {
+            'src': 'images/mobile/intalk/8.png',
+            'thumb': 'images/mobile/intalk/8.png',
+            'subHtml': "New Ticket"
+        }, {
+            'src': 'images/mobile/intalk/9.png',
+            'thumb': 'images/mobile/intalk/9.png',
+            'subHtml': "Change Password"
+        }, {
+            'src': 'images/mobile/intalk/10.png',
+            'thumb': 'images/mobile/intalk/10.png',
+            'subHtml': "Side Menu"
+        }]
+    })
+});
+
+
+
